52a521c5ccaa570ca5a273013f07d86740b8c765
nataliaSumali <natalia.sumali@gmail.com>
2014-10-11 19:51:19 -0700

    re-restart, successfully make x appear in each box when clicked

---
+++ main.js
@@ -0,0 +1,13 @@
+document.getElementById("c0").addEventListener("click", myFunction);
+document.getElementById("c1").addEventListener("click", myFunction);
+document.getElementById("c2").addEventListener("click", myFunction);
+document.getElementById("c3").addEventListener("click", myFunction);
+document.getElementById("c4").addEventListener("click", myFunction);
+document.getElementById("c5").addEventListener("click", myFunction);
+document.getElementById("c6").addEventListener("click", myFunction);
+document.getElementById("c7").addEventListener("click", myFunction);
+document.getElementById("c8").addEventListener("click", myFunction);
+
+function myFunction() {
+	this.innerHTML = "X";
+}
